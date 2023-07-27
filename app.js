00e3a3e33166b02d39622e298d8e355f5eab955e
derrickdrye <dryd95@hotmail.com>
2023-07-27 14:26:52 -0500

    JavaScript Variables Complete

--- app.js
+++ app.js
@@ -4,7 +4,34 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Derrick";
+let lastName = "Drye";
+var age = 47;
 
 // Exercise 2
+lastName = "Drye";
+age = 47;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(`${language} was first release in ${createdYear}.`);
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log(`This Tee Shirt size ${selectedSize} is ${price} with ${stock} in stock.`);
+
+// Exercise 5
+let title = "Of Mice and Men";
+let author = "John Steinbeck";
+let pageCount = 107;
+let bookmark = 56
+let hasRead = true;
